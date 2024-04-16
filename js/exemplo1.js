9109fc739d33d695c278963d8c78479af445b5a1
VINÍCIUS JOSÉ MATIAS MARQUES <viniciusboz1000@outlook.com>
2024-04-16 11:09:37 -0300

    Iniciando JS

---
+++ js/exemplo1.js
@@ -0,0 +1,8 @@
+function mostrarTexto(){
+    let paragrafo = document.getElementById("paragrafo");
+    let inputTexto = document.getElementById("campoTexto");
+
+    alert(paragrafo.textContent);
+    alert(inputTexto.value);
+    
+}
