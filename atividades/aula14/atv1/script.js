0a2e6d493b139f1ac1b61eee16f01db3f8b0655f
VINÍCIUS JOSÉ MATIAS MARQUES <viniciusboz1000@outlook.com>
2024-04-30 10:46:58 -0300

    Atividade 1 da aula 14 terminar

--- atividades/aula14/atv1/script.js
+++ atividades/aula14/atv1/script.js
@@ -1,5 +1,27 @@
-function adivinharNumero(){
-    let inputNumero = document.getElementById("inputNumero").value;
-    let btn = document.getElementById("btn");
+let inputNumero = document.getElementById("inputNumero");
+let btn = document.getElementById("btn");
+let novo = document.getElementById("novo_jogo");
+let resultado = document.getElementById("resultado");
 
-}
+let counter = 0;
+let secret = Math.floor(Math.random() * 50);
+console.log(secret)
+
+btn.addEventListener('click', () => {
+    if(inputNumero.value != secret && resultado < 3){
+        resultado.innerText = "Você Errou"
+    } else if (inputNumero.value != secret && resultado == 3) {
+        resultado.innerText = "Você perdeu!"
+    } else {
+        resultado.innerText = "Parabéns, você acertou!"
+
+    }
+    resultado += 1;
+})
+
+novo.addEventListener('click', () => {
+    inputNumero.value = 0
+    resultado = 0
+    secret = Math.floor(Math.random() * 50)
+    console.log(secret)
+})
